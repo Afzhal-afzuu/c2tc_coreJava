68bea7b36ab65e2d4a8883810ac9e9132ef7922b
ELCOT <ELCOT@Lenovo>
2023-08-28 08:41:47 +0530

    first commit

---
+++ afzz_java/src/com/afz/programs/demo.java
@@ -0,0 +1,15 @@
+package com.afz.programs;
+
+public class demo {
+
+	public static void main(String[] args)
+	{
+
+		float n1 = 10, n2 = 20, sum;
+		sum = n1 + n2;
+		System.out.println("first java program.");
+		System.out.println("sum of two numbers : "+ sum);
+		
+	}
+
+}
